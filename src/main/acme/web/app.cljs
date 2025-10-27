6d02c1e6e5c7168551f2935687a924f6859e2575
Hao Wu <wuhaoecho@gmail.com>
2025-10-27 21:26:27 +0800

    Add React UI with form and Bulma styling
    - Add Reagent and React dependencies
    - Create interactive form component with state management
    - Add Bulma CSS framework for styling
    - Update HTML with viewport meta tag and stylesheet
    - Implement React root mounting and hot reload support

--- src/main/acme/web/app.cljs
+++ src/main/acme/web/app.cljs
@@ -1,8 +1,37 @@
-(ns acme.web.app)
+(ns acme.web.app
+  (:require [reagent.core :as r]
+            [reagent.dom.client :as rdom]))
 
-(defn init []
-  (println "Hello Worldd!"))
- 
+(defonce form-data (r/atom {:name ""
+                            :email ""}))
+
+(defn handle-submit [event]
+  (.preventDefault event)
+  (js/console.log "Submitting form" (clj->js @form-data)))
+
+(defn text-input [id label field]
+  [:div
+   [:label {:for id} label]
+   [:input {:id id
+            :value (get @form-data field)
+            :on-change #(swap! form-data assoc field (.. % -target -value))}]])
+
+(defn form-view []
+  [:form {:on-submit handle-submit}
+   [text-input "name" "Name" :name]
+   [text-input "email" "Email" :email]
+   [:button {:type "submit"} "Submit"]])
+
+(defonce root-instance (atom nil))
+
+(defn mount-root []
+  (when-let [container (.getElementById js/document "root")]
+    (let [instance (or @root-instance
+                       (reset! root-instance (rdom/create-root container)))]
+      (rdom/render instance [form-view]))))
+
+(defn ^:export init []
+  (mount-root))
+
 (defn reload! []
-  (println "Code updated.")
-  )
+  (mount-root))
